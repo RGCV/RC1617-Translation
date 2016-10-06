11eb0096d3def0a6ee4e2c9e16e34baf88b7f869
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2016-10-06 02:01:31 +0100

    Generalize and move function implementations from user

--- src/rctr.c
+++ src/rctr.c
@@ -7,13 +7,17 @@
  * @author: Sara Azinhal (ist181700)
  */
 
+#include <errno.h>
+#include <netinet/in.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include "rctr.h"
 
 /* Function implementations */
-
 int eprintf(const char *format, ...) {
   int ret;
   va_list args;
@@ -23,4 +27,71 @@ int eprintf(const char *format, ...) {
   va_end(args);
 
   return ret;
+}
+
+ssize_t rwrite(int fd, char *buffer, ssize_t size) {
+  ssize_t offset = 0;
+
+  while(size > 0) {
+    ssize_t nbytes = 0;
+
+    if((nbytes = write(fd, buffer + offset, size - offset)) == -1) {
+      perror("write");
+      close(fd);
+      exit(E_GENERIC);
+    }
+
+    size -= nbytes;
+    offset += nbytes;
+  }
+
+  return offset;
+}
+
+ssize_t rread(int fd, char *buffer, ssize_t size) {
+  ssize_t offset = 0;
+
+  while(size > 0) {
+    ssize_t nbytes = 0;
+
+    if((nbytes = read(fd, buffer + offset, size - offset)) == -1) {
+      perror("read");
+      close(fd);
+      exit(E_GENERIC);
+    }
+
+    size -= nbytes;
+    offset += nbytes;
+  }
+
+  return offset;
+}
+
+int udp_send_recv(int sockfd, void *buf, size_t len, size_t size,
+    struct sockaddr *dest_addr, socklen_t *addrlen, unsigned long delay) {
+  int err = EXIT_SUCCESS;
+
+  if(sendto(sockfd, buf, len, 0, dest_addr, *addrlen) == -1) {
+    err = errno;
+    perror("sendto");
+  }
+
+  if(delay > 0) {
+    struct timeval tv;
+
+    tv.tv_sec = delay;
+    tv.tv_usec = 0;
+    if(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (void *)&tv,
+        (socklen_t)sizeof(struct timeval)) == -1) {
+      err = errno;
+      perror("setsockopt");
+    }
+  }
+
+  if(!err && recvfrom(sockfd, buf, size, 0, dest_addr, addrlen) == -1) {
+    err = errno;
+    perror("recvfrom");
+  }
+
+  return err;
 }
