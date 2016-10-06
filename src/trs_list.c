d064de4546c538b30436b5a15eb360672b514568
Rui Ventura <RGCV@users.noreply.github.com>
2016-10-06 08:53:49 +0100

    Add implementation file

---
+++ src/trs_list.c
@@ -0,0 +1,15 @@
+/**
+ * RC Translation - RC@IST/UL
+ * User client - RC45179 15'16
+ *
+ * @author: Rui Ventura (ist181045)
+ * @author: Diogo Freitas (ist181586)
+ * @author: Sara Azinhal (ist181700)
+ */
+
+#include "rctr.h"
+#include "trs_list.h"
+
+struct trs_entry {
+  char language[LANG_MAX_LEN];
+};
